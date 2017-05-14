a5424289a797d64c06cf49b7fa33168d7290c143
spiddy <dimitris.kapanidis@harbur.io>
2017-05-14 11:21:52 +0200

    Remove comments

--- server.go
+++ server.go
@@ -9,7 +9,6 @@ import (
 )
 
 var target_url string = os.Getenv("TARGET_URL")
-//var target_url string = "http://127.0.0.1:8080"
 
 /** Agggregated snapshot information of one second **/
 type Snapshot struct {
